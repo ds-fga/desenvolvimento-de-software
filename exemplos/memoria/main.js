090a83f52eb9976c39ebc41f78517fd79e6c36f4
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-05-16 23:12:13 -0300

    Lógica inicial: jogando de 2 em 2, removendo o eventListener ao acertar um par, e virando o par caso contrário

--- exemplos/memoria/main.js
+++ exemplos/memoria/main.js
@@ -1,9 +1,39 @@
 let estado = {
-  selecionado: null
+  flipouCarta: false,
+  primeiraCarta: null,
+  segundaCarta: null,
 };
 
+function jogo(event) {
+
+  const cartaAtual = $(event.target);
+
+  cartaAtual.toggleClass("costas");
+
+  if(!estado.flipouCarta) { // primeiro click para tentar achar o par
+    estado.flipouCarta = true;
+    estado.primeiraCarta = cartaAtual;
+  }
+  else { // segundo click para tentar achar o par
+    estado.flipouCarta = false;
+    estado.segundaCarta = cartaAtual;
+    console.log(estado.flipouCarta, estado.segundaCarta);
+    
+    if(estado.primeiraCarta.data("tipocarta") === estado.segundaCarta.data("tipocarta")) {
+      console.log("iguais");
+      estado.primeiraCarta.off('click', jogo);
+      estado.segundaCarta.off('click', jogo);
+    }
+    else {
+      estado.primeiraCarta.addClass("costas");
+      estado.segundaCarta.addClass("costas");
+      console.log('oi');
+    }
+  }
+
+}
+
 $(() => {
-  $(".cartao").on("click", ev => {
-    $(ev.target).toggleClass("costas");
-  });
+
+  $(".cartao").on("click", jogo);
 });
