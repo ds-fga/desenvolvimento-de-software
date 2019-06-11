06c762e5510d6fa771d5a41366f851f2c55a572a
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2019-06-11 14:32:44 -0300

    Adicioun exemplo de perfis

---
+++ provas/perfis.py
@@ -0,0 +1,17 @@
+PERFIS = [
+    ('Perfil 1', [
+        {2: 1},           # 1 ponto para opção 2 e 0 para o resto
+        {0: -0.5, 1: 1},  # -0.5 pontos para a opção 0 e 1 ponto para opção -1
+        {1: 1},
+    ]),
+    ('Perfil 2', [
+        {1: 1},          
+        {2: 1},  
+        {0: 1},
+    ]),
+    ('Perfil 3', [
+        {0: 1},          
+        {0: 1},  
+        {2: 1},
+    ]),
+]
